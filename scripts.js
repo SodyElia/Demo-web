91b9582e9172470c7694ddd217530de542d67de7
SodyElia <eliamgeusody@yahoo.com>
2024-07-19 23:42:02 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdown = document.querySelector('.dropdown');
+
+    dropdown.addEventListener('mouseover', function() {
+        this.querySelector('.dropdown-content').style.display = 'block';
+    });
+
+    dropdown.addEventListener('mouseout', function() {
+        this.querySelector('.dropdown-content').style.display = 'none';
+    });
+});
